d780136237e5952bed6bd27e13d3733b53627902
Edouard CAFFIER <cafed@MacBook-Air-de-Edouard.local>
2021-05-04 22:18:18 +0200

    first commit new computer

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 import Header from './components/layout/header/Header'
+import Project from './components/layout/body/Project'
 
 function App() {
   return (
     <div>
       <Header />
+      <Project />
     </div>
   );
 }
---
+++ src/components/layout/body/Project.js
@@ -0,0 +1,9 @@
+function Project (){
+    return(
+        <div className="container">
+            <h1 className="text-center"> Mes projets</h1>
+            
+        </div>
+    );
+}
+export default Project;
